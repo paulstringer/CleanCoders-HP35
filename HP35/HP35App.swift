8a719c60d5c03938aed665e662b932572fbfaab5
paulstringer <pstringer@equalexperts.com>
2021-02-20 13:23:24 +0000

    Initial Commit

---
+++ HP35/HP35App.swift
@@ -0,0 +1,17 @@
+//
+//  HP35App.swift
+//  HP35
+//
+//  Created by paulstringer on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct HP35App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
