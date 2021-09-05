73dd2773ed5ba5afb58eb15247d311de4981264e
paulstringer <pstringer@equalexperts.com>
2021-09-05 11:04:26 +0100

    [DEV] Add simple UI
    Other:
    
    - Modifies scheme to run Fitnesse in Browser upon build/test

--- HP35/ContentView.swift
+++ HP35/ContentView.swift
@@ -1,16 +1,157 @@
-//
-//  ContentView.swift
-//  HP35
-//
-//  Created by paulstringer on 20/02/2021.
-//
-
 import SwiftUI
+
+struct CalcButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .lineLimit(1)
+            .font(.title.bold())
+            .minimumScaleFactor(0.5)
+    }
+}
+
+struct CalcButton: View {
+
+    let label: String
+    let action: () -> Void
+
+    init(_ label: String, action: @escaping () -> Void) {
+        self.label = label
+        self.action = action
+    }
+
+    var body: some View {
+        Button(label) {
+            action()
+        }
+        .buttonStyle(CalcButtonStyle())
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        let ROWSPACING: CGFloat = 20.0
+
+        VStack {
+
+            HStack {
+                Text("-1.23456789")
+                .lineLimit(1)
+                .font(.largeTitle)
+                .foregroundColor(.red)
+
+                Spacer()
+
+                Text("-35")
+                .lineLimit(1)
+                    .font(.largeTitle)
+                .foregroundColor(.red)
+                .padding()
+            }
+
+            Spacer()
+
+            HStack(spacing:ROWSPACING) {
+
+                CalcButton("ENTER ⬆️") {
+                    //
+                }.fixedSize(horizontal: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/, vertical: false)
+
+                CalcButton("CH S") {
+                    //
+                }
+
+                CalcButton("E EX") {
+                    //
+                }
+
+                CalcButton("CL X") {
+                    //
+                }
+
+            }
+
+            HStack(spacing:ROWSPACING) {
+
+                CalcButton("-") {
+                    //
+                }
+
+                CalcButton("7") {
+                    //
+                }
+
+                CalcButton("8") {
+                    //
+                }
+
+                CalcButton("9") {
+                    //
+                }
+            }
+
+            HStack(spacing:ROWSPACING) {
+
+                CalcButton("+") {
+                    //
+                }
+
+                CalcButton("4") {
+                    //
+                }
+
+                CalcButton("5") {
+                    //
+                }
+
+                CalcButton("6") {
+                    //
+                }
+            }
+
+            HStack(spacing:ROWSPACING) {
+                CalcButton("x") {
+                    //
+                }
+
+                CalcButton("1") {
+                    //
+                }
+
+                CalcButton("2") {
+                    //
+                }
+
+                CalcButton("3") {
+                    //
+                }
+            }
+
+
+            HStack(spacing:ROWSPACING) {
+                CalcButton("÷") {
+                    //
+                }
+
+                CalcButton("0") {
+                    //
+                }
+
+                CalcButton(".") {
+                    //
+                }
+
+                CalcButton("π") {
+                    //
+                }
+                .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+            }
+
+        }
+        .padding(.all)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+
     }
 }
 
